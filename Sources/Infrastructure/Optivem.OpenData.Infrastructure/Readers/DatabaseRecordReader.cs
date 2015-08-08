aa9565ba910d8ceb4c43e97a752e93203b5baa20
Valentina Cupać <valentina.cupac@optivem.com>
2015-08-08 19:27:51 +0200

    Added readers for delimited text files and sql databases

---
+++ Sources/Infrastructure/Optivem.OpenData.Infrastructure/Readers/DatabaseRecordReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optivem.OpenData.Infrastructure.Readers
+{
+    /// <summary>
+    /// Used for reading database records from SQL Database
+    /// </summary>
+    public class DatabaseRecordReader : BaseReader<object[], SqlDataReader>
+    {
+        public DatabaseRecordReader(SqlDataReader reader, int length)
+            : base(reader)
+        {
+            this.Length = length;
+        }
+
+        public int Length { get; private set; }
+
+        public override object[] Read()
+        {
+            if (!Reader.Read())
+            {
+                return null;
+            }
+
+            object[] record = new object[Length];
+            Reader.GetValues(record);
+            return record;
+        }
+
+        public override List<object[]> ReadToEnd()
+        {
+            List<object[]> records = new List<object[]>();
+
+            object[] record = null;
+
+            while ((record = Read()) != null)
+            {
+                records.Add(record);
+            }
+
+            return records;
+        }
+    }
+}
---
+++ Sources/Infrastructure/Optivem.OpenData.Infrastructure/Readers/IReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optivem.OpenData.Infrastructure.Readers
+{
+    /// <summary>
+    /// Interface for reader
+    /// </summary>
+    /// <typeparam name="T">Type of the object being read</typeparam>
+    public interface IReader<T> : IDisposable
+    {
+        T Read();
+        List<T> ReadToEnd();
+    }
+}
